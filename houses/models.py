339c93b8e7b105f20fcbee479b562b3dd8cafe44
beom-yun <scds0326@naver.com>
2022-09-12 15:55:34 +0900

    Admin

--- houses/models.py
+++ houses/models.py
@@ -10,3 +10,6 @@ class House(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
