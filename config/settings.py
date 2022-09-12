f125b324eb3dc647bcef1f9eb8d63c0b5516f281
beom-yun <scds0326@naver.com>
2022-09-12 17:50:57 +0900

    Custom Model

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 CUSTOM_APPS = [
-    'houses.apps.HousesConfig'
+    'houses.apps.HousesConfig',
+    'users.apps.UsersConfig'
 ]
 
 SYSTEM_APPS = [
@@ -129,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Auth
+
+AUTH_USER_MODEL = 'users.User'
