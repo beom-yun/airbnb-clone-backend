460c9b4b873e5abd7478c7100b63158060cd3ddd
beom-yun <scds0326@naver.com>
2022-09-12 12:40:05 +0900

    Recap

--- houses/models.py
+++ houses/models.py
@@ -6,6 +6,7 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
