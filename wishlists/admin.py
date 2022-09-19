f2bf65396434fb9b7ade886d0ccdc448f62f8aa3
beom-yun <scds0326@naver.com>
2022-09-20 01:31:03 +0900

    Wishlists

---
+++ wishlists/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Wishlist
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "user",
+        "created_at",
+        "updated_at",
+    )
