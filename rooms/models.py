7bf4f418eb7ade1e93c28091fb06c146de506b66
beom-yun <scds0326@naver.com>
2022-09-26 21:10:52 +0900

    Admin Methods

--- rooms/models.py
+++ rooms/models.py
@@ -60,6 +60,9 @@ class Room(CommonModel):
     def __str__(self) -> str:
         return self.name
 
+    def total_amenities(self):
+        return self.amenities.count()
+
 
 class Amenity(CommonModel):
 
