5a9d0074dbfcc3af1041ca12a23cd236a1a02c33
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-10-15 21:14:31 +0900

    Create a Booking

--- rooms/views.py
+++ rooms/views.py
@@ -17,7 +17,7 @@ from categories.models import Category
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Amenities(APIView):
@@ -315,3 +315,11 @@ class RoomBookings(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
