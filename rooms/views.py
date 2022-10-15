e9b085eff370445e2767365de125f5b8651ce705
beom-yun <scds0326@naver.com>
2022-10-15 14:57:08 +0900

    Bookings

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,23 @@
-from multiprocessing.sharedctypes import Value
-from rest_framework.views import APIView
+from django.conf import settings
 from django.db import transaction
+from django.utils import timezone
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotFound,
-    NotAuthenticated,
     ParseError,
     PermissionDenied,
 )
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Amenity, Room
+from .serializers import AmenitySerializer, RoomDetailSerializer, RoomListSerializer
 from users.models import User
 from categories.models import Category
-from .serializers import AmenitySerializer, RoomDetailSerializer, RoomListSerializer
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
-from django.conf import settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from bookings.models import Booking
+from bookings.serializers import PublicBookingSerializer
 
 
 class Amenities(APIView):
@@ -289,3 +290,28 @@ class RoomPhotos(APIView):
             return Response(
                 serializer.errors,
             )
+
+
+class RoomBookings(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializer = PublicBookingSerializer(
+            bookings,
+            many=True,
+        )
+        return Response(serializer.data)
