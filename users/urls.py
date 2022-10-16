495f7b921b65ac52cb0e38f56fcfbb367df959ec
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-10-16 12:35:01 +0900

    Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Me, Users, PublicUser, ChangePassword
+from .views import Me, Users, PublicUser, ChangePassword, LogIn, LogOut
 
 urlpatterns = [
     path("", Users.as_view()),
     path("me", Me.as_view()),
     path("change-password", ChangePassword.as_view()),
+    path("log-in", LogIn.as_view()),
+    path("log-out", LogOut.as_view()),
     path("@<str:username>", PublicUser.as_view()),
 ]
