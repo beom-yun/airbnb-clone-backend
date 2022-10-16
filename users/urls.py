084afc1a08ffd933fa95b07ad9cc73cdc04d7361
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-10-16 11:59:03 +0900

    Create User

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Me
+from .views import Me, Users
 
 urlpatterns = [
     path("me", Me.as_view()),
+    path("", Users.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
-from http.client import ResponseNotReady
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.exceptions import ParseError
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .serializers import PrivateUserSerializer
@@ -24,6 +24,22 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
