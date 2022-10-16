cec97d7306d1af9cfb296db38b05664595771d72
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-10-16 12:21:08 +0900

    Change Password

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Me, Users
+from .views import Me, Users, PublicUser, ChangePassword
 
 urlpatterns = [
-    path("me", Me.as_view()),
     path("", Users.as_view()),
+    path("me", Me.as_view()),
+    path("change-password", ChangePassword.as_view()),
+    path("@<str:username>", PublicUser.as_view()),
 ]
