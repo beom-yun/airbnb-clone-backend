fd6b6de1babbaa5409d7dc3537414ceefd463b03
beom-yun <scds0326@naver.com>
2022-11-27 18:09:04 +0900

    #5.5 Foreign Keys

--- houses/models.py
+++ houses/models.py
@@ -10,6 +10,7 @@ class House(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
