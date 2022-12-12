9b455e1d206425d2cbe1038934d57a5ad24e307f
beom-yun <scds0326@naver.com>
2022-12-12 20:12:56 +0900

    #9.2 URL Arguments

--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def see_all_rooms(request):
+    return HttpResponse("See all rooms!")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"See one room with id: {room_id}")
