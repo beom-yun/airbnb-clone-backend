a1039dd03e174c278ec500a36a25c62c488fa396
beom-yun <scds0326@naver.com>
2022-12-13 00:14:00 +0900

    #10.1 JsonResponse

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category
 
-# Create your views here.
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse({"ok": True})
