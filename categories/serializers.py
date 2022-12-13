b69612f52cb731c48773945fdcd2dc4700917832
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-13 20:27:36 +0900

    #10.6 save()

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from .models import Category
 
 
 class CategorySerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=True, max_length=50)
-    kind = serializers.CharField(max_length=15)
+    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices)
     created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
