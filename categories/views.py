0507929cfa3246603f120e39d6b306067af29166
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-13 12:53:22 +0900

    #10.2 api_view

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,13 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    return Response(
+        {
+            "ok": True,
+            "categories": Category.objects.all(),
+        }
+    )
