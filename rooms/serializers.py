5904906fd5bf7b7deace04cc1b9bcb1163b971a3
beom-yun <scds0326@naver.com>
2022-12-16 23:56:14 +0900

    #11.6 Room Owner

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -27,8 +27,8 @@ class AmenitySerializer(ModelSerializer):
 
 class RoomDetailSerializer(ModelSerializer):
     owner = TinyUserSerializer(read_only=True)
-    amenities = AmenitySerializer(many=True)
-    category = CategorySerializer()
+    amenities = AmenitySerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Room
