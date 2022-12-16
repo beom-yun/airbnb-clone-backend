5169415e35e6a56e0760c3aa499ea264ee2e8f35
beom-yun <scds0326@naver.com>
2022-12-16 13:47:50 +0900

    Delete all

--- categories/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Category
-from .serializers import CategorySerializer
-
-
-class CategoryViewset(ModelViewSet):
-
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
--- experiences/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Perks, PerkDetail
-
-urlpatterns = [
-    path("perks/", Perks.as_view()),
-    path("perks/<int:pk>", PerkDetail.as_view()),
-]
