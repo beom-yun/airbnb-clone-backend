bd5966850e9c20152c6dfc42fda0077aeebb3a62
beom-yun <scds0326@naver.com>
2022-12-17 00:11:10 +0900

    #11.7 Room Category

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Room, Amenity
 from .serializers import RoomListSerializer, RoomDetailSerializer, AmenitySerializer
+from categories.models import Category
 
 
 class Rooms(APIView):
@@ -16,7 +17,16 @@ class Rooms(APIView):
         if request.user.is_authenticated:
             serializer = RoomDetailSerializer(data=request.data)
             if serializer.is_valid():
-                room = serializer.save(owner=request.user)
+                category_pk = request.data.get("category")
+                if not category_pk:
+                    raise ParseError
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                        raise ParseError
+                except Category.DoesNotExist:
+                    raise ParseError
+                room = serializer.save(owner=request.user, category=category)
                 serializer = RoomDetailSerializer(room)
                 return Response(serializer.data)
             else:
