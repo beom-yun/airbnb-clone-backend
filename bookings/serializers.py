19b24c148e7aee2cfa1636d85884e144ba34ba31
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-23 18:33:04 +0900

    #11.23 Create a Booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from django.utils import timezone
+from rest_framework.serializers import ModelSerializer, DateField, ValidationError
 from .models import Booking
+
+
+class CreateRoomBookingSerializer(ModelSerializer):
+
+    check_in = DateField()
+    check_out = DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise ValidationError("Can't book in the past!")
+        return value
 
 
 class PublicBookingSerializer(ModelSerializer):
--- rooms/views.py
+++ rooms/views.py
@@ -13,7 +13,7 @@ from users.models import User
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Rooms(APIView):
@@ -264,3 +264,11 @@ class RoomBookings(APIView):
         )
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
