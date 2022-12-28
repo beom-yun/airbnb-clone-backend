fc7c91331410cf37a15662f7aca753fe59fbaa1d
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-28 13:01:53 +0900

    #12.1 Create User

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from .serializers import PrivateUserSerializer
 
 
@@ -18,6 +19,23 @@ class Me(APIView):
         serializer = PrivateUserSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
