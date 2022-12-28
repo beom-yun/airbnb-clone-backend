3c008de0f9edad15423d608ad869a9db76eedd75
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-28 13:20:11 +0900

    #12.2 Change Password

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.status import HTTP_200_OK
 from .serializers import PrivateUserSerializer
+from .models import User
 
 
 class Me(APIView):
@@ -40,3 +42,31 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise NotFound
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=HTTP_200_OK)
+        else:
+            raise ParseError
