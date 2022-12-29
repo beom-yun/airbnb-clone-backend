f99a8082137297b51af1a5249b296481ce286cca
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-12-29 21:25:25 +0900

    #15.6 Environment Files

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i6(ri+fi%gb77)4$j!lgs#-g*6h#%gfgb4)u81=x@p55=iiuz$"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
