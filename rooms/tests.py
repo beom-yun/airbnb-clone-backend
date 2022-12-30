d4adc035e67d9bde5d83006675c4e0219a2cb17e
beom-yun <scds0326@naver.com>
2022-12-30 12:51:08 +0900

    #16.2 Amenities Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,6 +1,21 @@
 from rest_framework.test import APITestCase
+from .models import Amenity
 
 
 class TestAmenities(APITestCase):
-    def test_two_plus_two(self):
-        self.assertEqual(2 + 2, 4, "The math is wrong.")
+
+    NAME = "Amenity Test"
+    DESC = "Amenity Description"
+
+    def setUp(self):
+        Amenity.objects.create(name=self.NAME, description=self.DESC)
+
+    def test_all_amenities(self):
+        response = self.client.get("/api/v1/rooms/amenities/")
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200, "Status code isn't 200.")
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["name"], self.NAME)
+        self.assertEqual(data[0]["description"], self.DESC)
