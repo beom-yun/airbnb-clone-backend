8efc5a52cbece2611c60b1d009b8333605ae3ea4
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-09 14:15:52 +0900

    Defaults

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
