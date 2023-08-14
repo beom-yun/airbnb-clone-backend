edeec74a55877234c2438c887eb98e31486f8449
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-14 20:45:38 +0900

    All Amenities

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.see_all_rooms),
-    path("<int:room_id>", views.see_one_rooms),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>/", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,31 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from .models import Room, Amenity
+from .serializers import AmenitySerializer
 
 
-def see_all_rooms(request):
-    return HttpResponse("All rooms!")
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            new_amenity = serializer.save()
+            return Response(AmenitySerializer(new_amenity).data)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-def see_one_rooms(request, room_id):
-    return HttpResponse(f"Room #{room_id}")
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
