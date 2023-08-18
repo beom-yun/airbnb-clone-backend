1d26c0053361b4544e89500e1a563843b66f266c
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-19 00:26:03 +0900

    Wishlist

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Wishlists.as_view()),
+    path("<int:pk>/", views.WishlistDetail.as_view()),
+    path("<int:pk>/rooms/<int:room_pk>/", views.WishlistToggle.as_view()),
 ]
--- wishlists/views.py
+++ wishlists/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from .models import Wishlist
 from .serializers import WishlistSerializer
+from rooms.models import Room
 
 
 class Wishlists(APIView):
@@ -24,3 +25,58 @@ class Wishlists(APIView):
             return Response(WishlistSerializer(new_wishlist).data)
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class WishlistDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        return Response(WishlistSerializer(wishlist, context={"request": request}).data)
+
+    def put(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist, data=request.data, partial=True, context={"request": request}
+        )
+        if serializer.is_valid():
+            wishlist = serializer.save()
+            serializer = WishlistSerializer(wishlist, context={"request": request})
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        wishlist.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+class WishlistToggle(APIView):
+    def get_list(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get_room(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_list(pk, request.user)
+        room = self.get_room(room_pk)
+        print(wishlist, room)
+        if wishlist.rooms.filter(pk=room.pk).exists():
+            wishlist.rooms.remove(room)
+        else:
+            wishlist.rooms.add(room)
+        return Response()
