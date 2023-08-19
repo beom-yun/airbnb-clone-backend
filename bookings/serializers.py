91a14634c493d9a98c80235ff6b01f80a995e45c
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-19 13:28:06 +0900

    Validate Booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -27,6 +27,19 @@ class CreateRoomBookingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Can't book in the past!")
         return value
 
+    def validate(self, data):
+        if data["check_out"] <= data["check_in"]:
+            raise serializers.ValidationError(
+                "Check in should be smaller than check out."
+            )
+        if Booking.objects.filter(
+            check_in__lt=data["check_out"], check_out__gt=data["check_in"]
+        ).exitsts():
+            raise serializers.ValidationError(
+                "Those or some of those dates are already taken."
+            )
+        return data
+
 
 class PublicBookingSerializer(serializers.ModelSerializer):
     class Meta:
