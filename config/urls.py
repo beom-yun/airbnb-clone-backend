862354bcda512a7b60a4ffdff176e942fff58e51
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-19 15:34:03 +0900

    User Profile

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("api/v1/experiences/", include("experiences.urls")),
     path("api/v1/medias/", include("medias.urls")),
     path("api/v1/wishlists/", include("wishlists.urls")),
+    path("api/v1/users/", include("users.urls")),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -10,3 +10,19 @@ class TinyUserSerializer(ModelSerializer):
             "username",
             "avatar",
         )
+
+
+class PrivateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "id",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
+        )
