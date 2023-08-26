4e83789f9900e6d0115348d684d2ad29c4ca640f
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-08-27 00:53:48 +0900

    Kakao Log In

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("change-password/", views.ChangePassword.as_view()),
     path("log-in/", views.LogIn.as_view()),
     path("log-out/", views.LogOut.as_view()),
+    path("kakao/", views.KakaoLogIn.as_view()),
     path("@<str:username>/", views.PublicUser.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
+import requests
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError, NotFound
 from .serializers import PrivateUserSerializer
@@ -91,3 +92,49 @@ class LogOut(APIView):
     def post(self, request):
         logout(request)
         return Response({"ok": "bye!"})
+
+
+class KakaoLogIn(APIView):
+    def post(self, request):
+        try:
+            code = request.data.get("code")
+            access_token = requests.post(
+                "https://kauth.kakao.com/oauth/token",
+                headers={
+                    "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
+                },
+                data={
+                    "grant_type": "authorization_code",
+                    "client_id": "02fb363a83261884d07ac2ecc9372288",
+                    "redirect_uri": "http://127.0.0.1:3000/social/kakao/",
+                    "code": code,
+                },
+            )
+            access_token = access_token.json().get("access_token")
+            user_data = requests.get(
+                "https://kapi.kakao.com/v2/user/me",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Content-type": "Content-type: application/x-www-form-urlencoded;charset=utf-8",
+                },
+            )
+            user_data = user_data.json()
+            kakao_account = user_data.get("kakao_account")
+            profile = kakao_account.get("profile")
+            try:
+                user = User.objects.get(email=kakao_account.get("email"))
+                login(request, user)
+                return Response(status=HTTP_200_OK)
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    email=kakao_account.get("email"),
+                    username=profile.get("nickname"),
+                    name=profile.get("nickname"),
+                    avatar=profile.get("profile_image_url"),
+                )
+                user.set_unusable_password()
+                user.save()
+                login(request, user)
+                return Response(status=HTTP_200_OK)
+        except Exception:
+            return Response(status=HTTP_400_BAD_REQUEST)
